fe45aeebed6b11918e06003494b2f1991efe9c8f
Paya Mathilde <mathilde.paya@student-cs.fr>
2023-01-23 14:57:48 +0100

    added basic functions

---
+++ fonctionsutiles.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def readcsv(fichier):
+    
+    file = pd.read_csv(fichier)
+    return file
+
+def dataframegraph(fichier):
+    file = readcsv(fichier)
+    newfile = file[['id_user','id_followers']]
+
+    dico = {'id': [], 'follow': []}
+    for index, row in newfile.iterrows():
+        listefollowers = row['id_followers']
+        rowid = row['id_user']
+        string = listefollowers[1:-1]
+        for follower in list(string.split(', ')):    
+            dico['id'].append(rowid)
+            dico['follow'].append(int(follower))        
+    newnewfile = pd.DataFrame(dico) 
+    
+    return newnewfile
+    
+    
+
