0f87f6b6c80192d94657a9796ee7de0622badcef
Pietro <pietro.izzo@student-cs.fr>
2023-01-25 08:58:24 +0100

    Small changes

--- simulation.py
+++ simulation.py
@@ -1,1 +1,12 @@
 # This file (main?) will use the other ones to simulate the spreading process
+
+def update(dico,g):
+    #Prend en argument le dictionnaire de personnes à un instant et le réseau et fait le aps de temps suivant
+    for person in dico:
+        if dico[person]['is_posting'] == 1:
+            post(person,g)
+    
+        
+            
+def post(person,network):
+    
