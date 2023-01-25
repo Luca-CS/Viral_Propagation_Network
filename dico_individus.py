5e5d7003da5864acc1e7b77af71aaad56216ec95
Peyrot Lucie <lucie.peyrot@student-cs.fr>
2023-01-25 09:43:43 +0100

    age et nbr de reposts

--- dico_individus.py
+++ dico_individus.py
@@ -5,7 +5,7 @@ import random
 
 account = cleaned_account
 posts = post_df
-posts = posts.drop(index = 1129)
+
 
 # Creation of a dictionary with each user id and information of the user
 
@@ -32,7 +32,7 @@ def dico_tot(p):  # p : percentage of followers who saw the post
 
         if row.id_post_origin == 0:
             dico[row.id_user]['original'] = True
-        
+
         dico[row.id_user]['repost'] += 1
         followers = dico[row.id_user]['id_followers']
         n = round(len(followers)*p*0.01)  # Number of people who saw the post
@@ -51,3 +51,57 @@ def test(p):
         rep += dico[row]['repost']
     return rep
 
+
+# Number of people with a certain age who reposted
+def age_posts():
+    dico = dico_tot(100)
+    age_post = []
+    for row in dico:
+        if dico[row]['repost'] != 0:
+            age_post.append(int(dico[row]['age']))
+    age_post.sort()
+
+    liste_age = []
+    for i in range(age_post[len(age_post)-1] + 1):
+        liste_age.append(i)
+    liste_nombre = (age_post[len(age_post)-1]+1)*[0]
+    for x in age_post:
+        liste_nombre[x] += 1
+    return(liste_age, liste_nombre)
+
+
+liste_age, liste_nombre = age_posts()
+plt.plot(liste_age, liste_nombre)
+plt.show()
+
+# Number of reposts
+
+
+def nbr_reposts():
+    dico = dico_tot(100)
+    repost = []
+    for row in dico:
+        if int(dico[row]['repost']) != 0:
+            repost.append(int(dico[row]['repost']))
+    repost.sort()
+    print(repost)
+    n = len(repost)
+    max = repost[n-1]
+    min = repost[0]
+    if min == max:
+        return([min], [len(repost)])
+    else:
+        nbr_people = [0]*(max)
+        nbr_repost = []
+        for i in range(1, max + 1):
+            nbr_repost.append(i)
+        for x in repost:
+            nbr_people[x-1] += 1
+        return(nbr_repost, nbr_people)
+
+
+print(nbr_reposts())
+
+nbr_repost, nbr_people = nbr_reposts()
+plt.plot(nbr_repost, nbr_people)
+plt.show()
