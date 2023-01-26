1b92c15ed1723e501d71b979f73beedfe7aad2a3
Paya Mathilde <mathilde.paya@student-cs.fr>
2023-01-26 15:36:33 +0100

    fonction get kpi

--- kpi.py
+++ kpi.py
@@ -14,3 +14,14 @@ print([sumviews,sumlikes,sumcomments,sumreposts,sumlinks,sumdonations])
 
 plt.plot([sumviews,sumlikes,sumcomments,sumreposts,sumlinks,sumdonations])
 plt.show()
+
+def get_kpi(post_id):
+    row = post_df[post_df['id_post']==post_id]
+    if str(row['donation_tag'])=='TRUE':
+        don = 1
+    else : 
+        don = 0
+    kpi = int(row['views'])/251167 + int(row['likes'])/68109 + int(row['comments'])/12730 + int(row['reposts'])/3043 + int(row['link_clicks'])/320 + don/31
+    return kpi
+
+print(get_kpi(953043456))
