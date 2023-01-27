8958b4d8850a4033865235eeae56ec8bca7d89c1
Pietro <pietro.izzo@student-cs.fr>
2023-01-27 09:44:18 +0100

    Improved readability

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ST2-ViralMarketing-Groupe1
 
-Our group worked on viral marketing campaing's strategies to spread a piece of content on social networks.
+Our group worked on viral marketing campaing's strategies to spread a piece of content on social networks and make it viral.
 
 ## Group's member
 
@@ -28,7 +28,7 @@ pip3 install -r requirements.txt
 
 Artefact is a digital marketing agency created in 1998 with more than 25 locations in 18 countries. It's headquarters are located in Paris.
 
-Greenpeace has reiched out to Artefact in order to conceiv a viral marketing compaign in order to raise awarness about climate change.
+Greenpeace has reached out to Artefact in order to conceive a viral marketing compaign in order to raise awarness about climate change.
 
 A fake company called Orizon was created. Its proposal is to find houses that will be located on the futur waterfront by 2100 due to the rizing level of oceans. It supposedly uses data from IPCC ( Intergovernmental Panel on Climate Change) and NASA ( National aeronautics and space administration).
 
@@ -43,6 +43,17 @@ Our goal is to exploit the given dataset in order to acheive several goals:
 - understand the underlying mecanism behind the viral spread of a post on social networks.
 - come up with a refinement of the strategy to improve its efficiency
 - estimate the pros and cons of the new strategy
+
+# Files :
+- randomsim.ipynb : Jupyter notebook to generate a viral campaign on a random network
+- simulation.ipynb : Jupyter notebook to generate a viral campaign on the network used by the "Orizon" one
+- fonctionsutiles.py : Useful functions used to analyse the provided dataframes
+- kpi.py : used to calculate our KPI
+- graph.py : used to recreate the graph from the "Orizon" campaign
+- dico_individus.py : generate a dictionnary containing the information of the users in the dataframes
+- analyseposts.py : used to generate different graphs
+- data : folder containing all the dataframes used
+
 
 
 
--- analyseposts.py
+++ analyseposts.py
@@ -1,7 +1,13 @@
+# Generation of all used graphs
+
 from fonctionsutiles import *
+from dico_individus import *
+from kpi import *
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as st
+import math as ma
+
 
 '''
 #reposts par rapport aux views
@@ -270,4 +276,149 @@ plt.legend()
 plt.show()
 '''
 
-print(np.random.uniform(0,20))
+
+
+"""l0 = [638779430]
+l1 = [953043456]
+l2 = [144265898]
+l3 = [650889385]
+
+for index, row in post_df.iterrows():
+    if row.id_post_origin in l0:
+        l0.append(row.id_post)
+    if row.id_post_origin in l1:
+        l1.append(row.id_post)
+    if row.id_post_origin in l2:
+        l2.append(row.id_post)
+    if row.id_post_origin in l3:
+        l3.append(row.id_post)
+
+# calcul du kpi par post originel
+
+
+def kpi(liste):
+    kpi, views, likes, comments, reposts, clicks, donations = 0, 0, 0, 0, 0, 0, 0
+
+    for id in liste:
+        row = post_df[post_df['id_post'] == id]
+        views += int(row['views'])
+        likes += int(row['likes'])
+        comments += int(row['comments'])
+        reposts += int(row['reposts'])
+        clicks += int(row['link_clicks'])
+        if str(row['donation_tag']) == 'TRUE':
+            donations += 1
+
+    kpi = views/251167 + likes/68109 + comments / \
+        12730 + reposts/3043 + clicks/320 + donations/31
+    return kpi
+
+kpi0 = kpi(l0)
+kpi1 = kpi(l1)
+kpi2 = kpi(l2)
+kpi3 = kpi(l3)
+
+
+
+p0 = int(post_df[post_df['id_post'] == l0[0]]['id_user'])
+p1 = int(post_df[post_df['id_post'] == l1[0]]['id_user'])
+p2 = int(post_df[post_df['id_post'] == l2[0]]['id_user'])
+p3 = int(post_df[post_df['id_post'] == l3[0]]['id_user'])
+print(p0)
+personnes = [p0,p1,p2,p3]
+
+genre = []
+
+
+for p in personnes:
+    genre.append(cleaned_account[cleaned_account['id_user']==p]['sex'])
+
+print(genre)
+
+
+activite = []
+
+for p in personnes:
+    activite.append(int(cleaned_account[cleaned_account['id_user']==p]['nb_posts']))
+    
+    
+followers = []
+
+for p in personnes:
+    followers.append(int(cleaned_account[cleaned_account['id_user']==p]['nb_followers']))
+    
+age = []
+
+for p in personnes:
+    age.append(int(cleaned_account[cleaned_account['id_user']==p]['age']))
+
+listekpi = [kpi0,kpi1,kpi2,kpi3]
+#plt.scatter([1,2,3,4],listekpi)
+#plt.scatter([1,2,3,4],activite)
+#plt.scatter([1,2,3,4],followers)
+#plt.scatter([1,2,3,4],age)
+plt.xlabel('original poster')
+plt.ylabel('total kpi')
+plt.show()"""
+
+'''
+X = [0, 1, 2, 3]
+Y = [kpi(l0), kpi(l1), kpi(l2), kpi(l3)]
+print(Y)
+plt.scatter(X, Y)
+plt.show()
+
+
+# import exponential
+
+# Using exponential() method
+gfg = 100*np.random.exponential(1/6, 10000)
+
+count, bins, ignored = plt.hist(gfg, 14, density=True)
+plt.show()
+'''
+# Génère des reposts:
+
+'''
+def influ_repost():
+    dico = dico_tot(100)
+    age = []
+    nbr_people_age = 43*[0]
+    esperance_kpi = 43*[0]
+    for n in range(13, 57):
+        age.append(n)
+    for index, row in post_df.iterrows():
+        id = row.user_id
+        user_age = dico[id][age]
+        nbr_people_age[user_age - 13] += 1
+        esperance_kpi[user_age - 13] += get_kpi(int(row.post_id))
+    for x in range(43):
+        esperance_kpi[x] = esperance_kpi[x]/nbr_people_age[x]
+    plt.scatter(age, esperance_kpi)
+    plt.show
+
+
+def influ_genre():
+    dico = dico_tot(100)
+    genre = [1, 2]
+    nbr_people_genre = [0, 0]
+    esperance_kpi = [0, 0]
+    for index, row in post_df.iterrows():
+        id = row.id_user
+        if id in dico :
+            if dico[id]['gender'] == 'female':
+                nbr_people_genre[1] += 1
+                esperance_kpi[1] += get_kpi(int(row.id_post))
+            else:
+                nbr_people_genre[0] += 1
+                esperance_kpi[0] += get_kpi(int(row.id_post))
+    esperance_kpi[0] *= 1/nbr_people_genre[0]
+    esperance_kpi[1] *= 1/nbr_people_genre[1]
+    return esperance_kpi,nbr_people_genre
+    
+X,Y = influ_genre()
+print(X)
+print(Y)
+plt.scatter(X,Y)
+plt.show()
+'''
